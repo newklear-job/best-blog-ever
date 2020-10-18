8b342a894aba783c6888ddd61835a3ab0e86541c
Vitalik <petergerinv@gmail.com>
2020-10-18 19:06:36 +0300

    Vue cli, create templates.

---
+++ vuejs/src/layouts/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav-header/>
+  <app-header/>
+  <nav-topic/>
+  <router-view/>
+  <app-footer/>
+</template>
+
+<script>
+import NavHeader from './components/NavHeader'
+import AppHeader from './components/AppHeader'
+import NavTopic from './components/NavTopic'
+import AppFooter from './components/AppFooter'
+
+export default {
+  name: 'Index',
+  components: { NavHeader, AppHeader, NavTopic, AppFooter }
+}
+</script>
---
+++ vuejs/src/layouts/components/NavHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="w-full py-4 bg-blue-800 shadow">
+    <div
+      class="w-full container mx-auto flex flex-wrap items-center justify-between"
+    >
+      <nav>
+        <ul
+          class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline"
+        >
+          <li>
+            <router-link class="hover:text-gray-200 hover:underline px-4" to="/"
+              >Home</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              class="hover:text-gray-200 hover:underline px-4"
+              to="/post"
+              >Post</router-link
+            >
+          </li>
+          <li>
+            <router-link
+              class="hover:text-gray-200 hover:underline px-4"
+              to="/about"
+              >About</router-link
+            >
+          </li>
+        </ul>
+      </nav>
+
+      <div class="flex items-center text-lg no-underline text-white pr-6">
+        <a class="" href="#">
+          <i class="fab fa-facebook"></i>
+        </a>
+        <a class="pl-6" href="#">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a class="pl-6" href="#">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a class="pl-6" href="#">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
